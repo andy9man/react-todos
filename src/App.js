cda437e1a7a68b713e0f0769083b7d38eade6e5d
Andrew N <aninneman@gmail.com>
2018-01-25 17:14:05 -0600

    Updating existing todo items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const createTodoObj = (title,completed) => { 
-  return ({ todoTitle: title,
-           completed: completed
+const createTodoObj = (title) => {
+  return ({
+    todoTitle: title,
+    completed: false,
+    edit: false
   });
 }
 
@@ -14,22 +16,49 @@ class App extends Component {
     this.state = {
       newTodo: '',
       todos: [ ] // todoTitle '', completed boolean
-      
+
     }
 
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCheckChange = this.handleCheckChange.bind(this);
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    this.handleDoubleClick = this.handleDoubleClick.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   handleInputChange(e) {
     this.setState( {newTodo: e.target.value} );
-
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    this.setState( {todos: this.state.todos.concat([createTodoObj(this.state.newTodo, false)]), newTodo: ''} );
-    
+    this.setState( {todos: this.state.todos.concat([createTodoObj(this.state.newTodo)]), newTodo: ''} );
+  }
+
+  handleCheckChange(todoID) {
+    const newArray = this.state.todos.slice(); //this creates a COPY of the array, not a reference to this.state.todos
+    newArray[todoID].completed = !newArray[todoID].completed;
+    this.setState( {todos: newArray});
+  }
+
+  handleDeleteClick(todoID) {
+    if (todoID !== -1) {
+      this.setState( {todos: this.state.todos.slice().splice(todoID, 1)} );
+    }
+  }
+
+  handleDoubleClick(todoID) {
+    const newArray = this.state.todos.slice();
+    newArray[todoID].edit = true;
+    this.setState( {todos: newArray} );
+  }
+
+  handleUpdateItem(todoID, newTitle) {
+    const newArray = this.state.todos.slice();
+    newArray[todoID].todoTitle = newTitle;
+    newArray[todoID].edit = false;
+    this.setState( {todos: newArray} );
   }
 
 
@@ -53,18 +82,50 @@ class App extends Component {
             </form>
           </div>
         {
-          this.state.todos.map( (todo, index) => {  
+          this.state.todos.map( (todo, index) => {
             return (
               <div className="row" key={index}>
-              <div> 
-                <input type="checkbox" />
-                <span> { todo.todoTitle } </span>
+              <div>
+                <input
+                  type="checkbox"
+                  onClick={() => {
+                    this.handleCheckChange(index);
+                  }}
+                />
+              </div>
+
+              <div>
+                {
+                  todo.edit ?
+                    <input
+                      type="text"
+                      defaultValue={todo.todoTitle}
+                      className="border"
+                      onBlur={ (e)  =>( this.handleUpdateItem(index, e.target.value)) }
+                      onKeyUp={ (e) => ( e.key === 'Enter' ? this.handleUpdateItem(index, e.target.value) : '' )}
+                    /> :
+                    <p
+                      style={ todo.completed ? {display: 'inline', textDecoration: 'line-through'} : {display: 'inline', textDecoration: 'none'} }
+                      onDoubleClick={ () => {
+                        this.handleDoubleClick(index);
+                      }}
+                  > { todo.todoTitle } </p>
+                }
+              </div>
+
+              <div>
+                <button
+                  className="delete"
+                  onClick={ () => {
+                    this.handleDeleteClick(index);
+                  }}
+                >X</button>
               </div>
             </div>
             );
           })
         }
-        
+
         <footer>
           <p style={ {marginBottom: '10px'} }>Double-click to edit a todo</p>
           <p>Created by Del, Angela, and Andy</p>
